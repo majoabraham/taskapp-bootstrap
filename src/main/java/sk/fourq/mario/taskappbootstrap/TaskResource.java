5af93b7e49487a43269961dd605ce4b20c6d1aee
Mario Abraham <mario.abraham@outlook.com>
2019-11-06 15:25:22 +0100

    Initial commit
    Signed-off-by: Mario Abraham <mario.abraham@outlook.com>

---
+++ src/main/java/sk/fourq/mario/taskappbootstrap/TaskResource.java
@@ -0,0 +1,85 @@
+/*
+ * Created by 4Q developer (dev@4q.sk)
+ * Copyright (c) 2019
+ * 4Q s.r.o. All rights reserved.
+ * http://www.4q.eu
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are not permitted.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package sk.fourq.mario.taskappbootstrap;
+
+import sk.fourq.bootstrap.search.FindParams;
+import sk.fourq.bootstrap.search.FindResult;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("task")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@ApplicationScoped
+public class TaskResource {
+
+    public TaskResource() {
+
+    }
+
+    @Inject
+    private TaskService taskService;
+
+    @GET
+    public FindResult<Task> getTasks() {
+
+        FindParams findParams = FindParams.create();
+        return taskService.findAll(findParams);
+    }
+
+    @Path("/{id}")
+    @GET
+    public Task getTask(@PathParam("id") Integer id) {
+
+        return taskService.find(id);
+    }
+
+    @POST
+    public void createTask(Task task) {
+
+        taskService.create(task);
+    }
+
+    @Path("/{id}")
+    @DELETE
+    public void deleteTask(@PathParam("id") Integer id) {
+
+        taskService.delete(id);
+    }
+
+    @PUT
+    public Task updateTask(Task task) {
+
+        return taskService.update(task);
+    }
+}
---
+++ target/generated-sources/annotations/sk/fourq/mario/taskappbootstrap/Task_.java
@@ -0,0 +1,16 @@
+package sk.fourq.mario.taskappbootstrap;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value = "org.hibernate.jpamodelgen.JPAMetaModelEntityProcessor")
+@StaticMetamodel(Task.class)
+public abstract class Task_ {
+
+	public static volatile SingularAttribute<Task, String> description;
+	public static volatile SingularAttribute<Task, Integer> id;
+	public static volatile SingularAttribute<Task, Boolean> done;
+
+}
+
